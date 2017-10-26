5b4ffad7340fa3ed9d637174fcd2a9c445aa6c84
vinjn <vinjn.z@gmail.com>
2017-10-26 10:07:42 +0800

    "Initial commit"

---
+++ src/MiniAreaScanApp.cpp
@@ -0,0 +1,37 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Log.h"
+
+#include "AssetManager.h"
+#include "MiniConfig.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class MiniAreaScanApp : public App
+{
+  public:
+    void setup() override
+    {
+        log::makeLogger<log::LoggerFile>();
+        createConfigUI({200, 200});
+    
+        getWindow()->getSignalKeyUp().connect([&](KeyEvent& event) {
+            if (event.getCode() == KeyEvent::KEY_ESCAPE) quit();
+        });
+        
+        getWindow()->getSignalDraw().connect([&] {
+            gl::clear();
+        });
+    }
+    
+private:
+};
+
+CINDER_APP( MiniAreaScanApp, RendererGl, [](App::Settings* settings) {
+    readConfig();
+    settings->setWindowSize(APP_WIDTH, APP_HEIGHT);
+    settings->setMultiTouchEnabled(false);
+} )
