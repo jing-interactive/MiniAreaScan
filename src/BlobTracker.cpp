f0b8e415758003f9ea06ad7b00e82f6e8884b0b5
vinjn <vinjn.z@gmail.com>
2018-11-21 22:28:48 +0800

    Fix

--- src/BlobTracker.cpp
+++ src/BlobTracker.cpp
@@ -6,26 +6,28 @@
 
 using std::vector;
 
-#define OPENCV_VERSION CVAUX_STR(CV_VERSION_MAJOR)""CVAUX_STR(CV_VERSION_MINOR)""CVAUX_STR(CV_VERSION_REVISION)
+#define OPENCV_VERSION          \
+    CVAUX_STR(CV_VERSION_MAJOR) \
+    "" CVAUX_STR(CV_VERSION_MINOR) "" CVAUX_STR(CV_VERSION_REVISION)
 
 #if defined _DEBUG
-#pragma comment(lib,"opencv_core"OPENCV_VERSION"d.lib")
-#pragma comment(lib,"opencv_imgproc"OPENCV_VERSION"d.lib")
-#pragma comment(lib,"opencv_features2d"OPENCV_VERSION"d.lib")
-#pragma comment(lib,"opencv_flann"OPENCV_VERSION"d.lib")
-#pragma comment(lib,"opencv_hal"OPENCV_VERSION"d.lib")
+#pragma comment(lib, "opencv_core" OPENCV_VERSION "d.lib")
+#pragma comment(lib, "opencv_imgproc" OPENCV_VERSION "d.lib")
+#pragma comment(lib, "opencv_features2d" OPENCV_VERSION "d.lib")
+#pragma comment(lib, "opencv_flann" OPENCV_VERSION "d.lib")
+#pragma comment(lib, "opencv_hal" OPENCV_VERSION "d.lib")
 #else
-#pragma comment(lib,"opencv_core"OPENCV_VERSION".lib")
-#pragma comment(lib,"opencv_imgproc"OPENCV_VERSION".lib")
-#pragma comment(lib,"opencv_features2d"OPENCV_VERSION".lib")
-#pragma comment(lib,"opencv_flann"OPENCV_VERSION".lib")
-#pragma comment(lib,"opencv_hal"OPENCV_VERSION".lib")
+#pragma comment(lib, "opencv_core" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_imgproc" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_features2d" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_flann" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_hal" OPENCV_VERSION ".lib")
 #endif
-#pragma comment(lib,"ippicvmt.lib")
+#pragma comment(lib, "ippicvmt.lib")
 
 using namespace cv;
 
-bool cmp_blob_area(const Blob& a, const Blob& b)
+bool cmp_blob_area(const Blob &a, const Blob &b)
 {
     return a.area > b.area;
 }
@@ -40,7 +42,7 @@ BlobFinder::Option::Option()
     handDistance = 0;
 }
 
-#define CVCONTOUR_APPROX_LEVEL  1   // Approx.threshold - the bigger it is, the simpler is the boundary
+#define CVCONTOUR_APPROX_LEVEL 1 // Approx.threshold - the bigger it is, the simpler is the boundary
 
 enum PointState
 {
@@ -51,7 +53,7 @@ enum PointState
     NEAR_NOTHING,
 };
 
-static PointState getPointState(const Point& pt, int width, int height)
+static PointState getPointState(const Point &pt, int width, int height)
 {
     int x1 = width;
     const int thresh = 3;
@@ -66,16 +68,16 @@ static PointState getPointState(const Point& pt, int width, int height)
     return NEAR_NOTHING;
 }
 
-void BlobFinder::execute(Mat& img, vector<Blob>& blobs, const BlobFinder::Option& option)
+void BlobFinder::execute(Mat &img, vector<Blob> &blobs, const BlobFinder::Option &option)
 {
     blobs.clear();
     static vector<Vec4i> hierarchy;
     static vector<vector<Point>> contours0;
     static vector<Point> approx;
 
-    findContours(img, contours0, hierarchy, RETR_EXTERNAL/*RETR_TREE*/, CHAIN_APPROX_SIMPLE);
+    findContours(img, contours0, hierarchy, RETR_EXTERNAL /*RETR_TREE*/, CHAIN_APPROX_SIMPLE);
 
-    for (const auto& contour : contours0)
+    for (const auto &contour : contours0)
     {
         bool isHole = false;
 
@@ -86,21 +88,21 @@ void BlobFinder::execute(Mat& img, vector<Blob>& blobs, const BlobFinder::Option
             if (option.convexHull) //Convex Hull of the segmentation
                 convexHull(contour, approx);
             else //Polygonal approximation of the segmentation
-                approxPolyDP(contour, approx, std::min<double>(length*0.003, 2.0), true);
+                approxPolyDP(contour, approx, std::min<double>(length * 0.003, 2.0), true);
 
             area = contourArea(approx); //update area
             Moments mom = moments(approx);
 
             blobs.push_back(Blob());
 
-            Blob& obj = blobs[blobs.size() - 1];
+            Blob &obj = blobs[blobs.size() - 1];
             //fill the blob structure
             obj.area = fabs(area);
             obj.length = length;
             obj.isHole = isHole;
             obj.box = boundingRect(approx);
             obj.rotBox = minAreaRect(approx);
-            obj.angle = (90 - obj.rotBox.angle)*GRAD_PI2;//in radians
+            obj.angle = (90 - obj.rotBox.angle) * GRAD_PI2; //in radians
 
             if (mom.m10 > -DBL_EPSILON && mom.m10 < DBL_EPSILON)
             {
@@ -125,7 +127,7 @@ void BlobFinder::execute(Mat& img, vector<Blob>& blobs, const BlobFinder::Option
         real_dist *= real_dist;
 
         // post-processing for fake hand tracking
-        for (auto& b : blobs)
+        for (auto &b : blobs)
         {
             std::set<PointState> NearPointSet;
 
@@ -152,15 +154,14 @@ void BlobFinder::execute(Mat& img, vector<Blob>& blobs, const BlobFinder::Option
             bool only_one_near = NearPointSet.size() == 1;
             int min_value = only_one_near ? 0 : INT_MAX;
 
-            for (int j = 0; j<b.pts.size(); j++)
+            for (int j = 0; j < b.pts.size(); j++)
             {
                 Point diff = b.pts[j] - pt_ref;
-                float dist = diff.x*diff.x + diff.y*diff.y;
+                float dist = diff.x * diff.x + diff.y * diff.y;
 
                 if (
                     (only_one_near && dist > min_value) ||
-                    (!only_one_near && dist < min_value)
-                    )
+                    (!only_one_near && dist < min_value))
                 {
                     min_value = dist;
                     min_idx = j;
@@ -174,7 +175,7 @@ void BlobFinder::execute(Mat& img, vector<Blob>& blobs, const BlobFinder::Option
             for (int j = 0; j < b.pts.size(); j++)
             {
                 Point diff = b.pts[j] - b.pts[min_idx];
-                if ((diff.x*diff.x + diff.y*diff.y) < real_dist)
+                if ((diff.x * diff.x + diff.y * diff.y) < real_dist)
                 {
                     sum_x += b.pts[j].x;
                     sum_y += b.pts[j].y;
@@ -196,7 +197,7 @@ BlobTracker::BlobTracker()
     IDCounter = 0;
 }
 
-void BlobTracker::trackBlobs(const vector<Blob>& newBlobs)
+void BlobTracker::trackBlobs(const vector<Blob> &newBlobs)
 {
     deadBlobs.clear();
     const int n_old = trackedBlobs.size();
@@ -204,8 +205,8 @@ void BlobTracker::trackBlobs(const vector<Blob>& newBlobs)
     vector<TrackedBlob> newTrackedBlobs(n_new);
     std::copy(newBlobs.begin(), newBlobs.end(), newTrackedBlobs.begin());
 
-    vector<int> nn_of_a(n_old);//nearest neighbor of pta in ptb
-    vector<int> dist_of_a(n_old);//nearest neighbor of pta in ptb
+    vector<int> nn_of_a(n_old);   //nearest neighbor of pta in ptb
+    vector<int> dist_of_a(n_old); //nearest neighbor of pta in ptb
     fill(nn_of_a.begin(), nn_of_a.end(), -1);
     fill(dist_of_a.begin(), dist_of_a.end(), INT_MAX);
 
@@ -230,7 +231,7 @@ void BlobTracker::trackBlobs(const vector<Blob>& newBlobs)
         const int n_matches = matches.size();
         for (int i = 0; i < n_matches; i++)
         {
-            const DMatch& match = matches[i];
+            const DMatch &match = matches[i];
             int t_id = match.trainIdx;
             int q_id = match.queryIdx;
             float dist = match.distance;
@@ -251,14 +252,14 @@ void BlobTracker::trackBlobs(const vector<Blob>& newBlobs)
         {
             //moving blobs
             Point2f lastCenter = trackedBlobs[i].center;
-            newTrackedBlobs[nn].id = trackedBlobs[i].id;//save id, cause we will overwrite the data
-            trackedBlobs[i] = newTrackedBlobs[nn];//update with new data
+            newTrackedBlobs[nn].id = trackedBlobs[i].id; //save id, cause we will overwrite the data
+            trackedBlobs[i] = newTrackedBlobs[nn];       //update with new data
 
             // TODO: ....
             trackedBlobs[i].velocity.x = trackedBlobs[i].center.x - lastCenter.x;
             trackedBlobs[i].velocity.y = trackedBlobs[i].center.y - lastCenter.y;
-            float posDelta = sqrtf((trackedBlobs[i].velocity.x*trackedBlobs[i].velocity.x) +
-                (trackedBlobs[i].velocity.y*trackedBlobs[i].velocity.y));
+            float posDelta = sqrtf((trackedBlobs[i].velocity.x * trackedBlobs[i].velocity.x) +
+                                   (trackedBlobs[i].velocity.y * trackedBlobs[i].velocity.y));
 
             // AlexP
             // now, filter the blob position based on MOVEMENT_FILTERING value
@@ -278,9 +279,9 @@ void BlobTracker::trackBlobs(const vector<Blob>& newBlobs)
         }
     }
     trackedBlobs.erase(remove_if(trackedBlobs.begin(), trackedBlobs.end(), std::mem_fun_ref(&TrackedBlob::isDead)),
-        trackedBlobs.end());
+                       trackedBlobs.end());
     //entering blobs
-    for (int i = 0; i<n_new; i++)
+    for (int i = 0; i < n_new; i++)
     {
         if (newTrackedBlobs[i].id == TrackedBlob::BLOB_NEW_ID)
         {
